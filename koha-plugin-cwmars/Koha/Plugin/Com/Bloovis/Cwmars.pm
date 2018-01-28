2c502dfc4aad9343e340b6753c27041819fae6fc
marka@pobox.com <marka@pobox.com>
2018-01-28 17:36:01 +0000

    Add C/W MARS Koha plugin.
    FossilOrigin-Name: 46b11e0e504d10874992f76d4043f388d08cb07603c812d0034e7099399be330

---
+++ koha-plugin-cwmars/Koha/Plugin/Com/Bloovis/Cwmars.pm
@@ -0,0 +1,124 @@
+package Koha::Plugin::Com::Bloovis::Cwmars;
+
+## It's good practive to use Modern::Perl
+use Modern::Perl;
+
+## Required for all plugins
+use base qw(Koha::Plugins::Base);
+
+## We will also need to include any Koha libraries we want to access
+use C4::Context;
+use C4::Members;
+use C4::Auth;
+use Koha::DateUtils;
+use Koha::Libraries;
+use Koha::Patron::Categories;
+use Koha::Account;
+use Koha::Account::Lines;
+use MARC::Record;
+use MARC::Batch;
+use Cwd qw(abs_path);
+use URI::Escape qw(uri_unescape);
+use LWP::UserAgent;
+
+## Here we set our plugin version
+our $VERSION = "1.0";
+
+## Here is our metadata, some keys are required, some are optional
+our $metadata = {
+    name            => 'C/W MARS Plugin',
+    author          => 'Mark Alexander',
+    date_authored   => '2018-01-28',
+    date_updated    => '2018-01-28',
+    minimum_version => '16.06.00.018',
+    maximum_version => undef,
+    version         => $VERSION,
+    description     => 'This plugin implements a MARC import format converter '
+      . 'for C/W MARS web page that contain MARC information.',
+};
+
+## This is the minimum code required for a plugin's 'new' method
+## More can be added, but none should be removed
+sub new {
+    my ( $class, $args ) = @_;
+
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+## The existence of a 'to_marc' subroutine means the plugin is capable
+## of converting some type of file to MARC for use from the stage records
+## for import tool
+##
+## This example takes a text file, each line of which contains a URL
+## pointing to a C/W Mars web page containing MARC information for
+## a holding, and converts each web page to the equivelent MARC record.
+
+sub cwmars {
+   my $url = shift;
+   my $filename = '/tmp/cwmars.marc';
+
+   unlink($filename);
+   system('/usr/local/bin/cwmars.rb', $url, $filename);
+
+   my $batch = MARC::Batch->new('USMARC', $filename);
+   $batch->strict_off();
+
+   my $marc = $batch->next;
+   return $marc;
+}
+
+sub to_marc {
+    my ( $self, $args ) = @_;
+
+    my $data = $args->{data};
+
+    my $batch = q{};
+
+    foreach my $line ( split( /\n/, $data ) ) {
+        chomp($line);
+	$line =~ s/\r//;	# Get rid of Windows newline cruft
+        my $record = cwmars($line);
+        $batch .= $record->as_usmarc() . "\x1D";
+    }
+
+    return $batch;
+}
+
+## If your plugin can process payments online,
+## and that feature of the plugin is enabled,
+## this method will return true
+sub opac_online_payment {
+    my ( $self, $args ) = @_;
+
+    return 0;
+}
+
+## This is the 'install' method. Any database tables or other setup that should
+## be done when the plugin if first installed should be executed in this method.
+## The installation method should always return true if the installation succeeded
+## or false if it failed.
+sub install() {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+## This method will be run just before the plugin files are deleted
+## when a plugin is uninstalled. It is good practice to clean up
+## after ourselves!
+sub uninstall() {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+1;
---
+++ koha-plugin-cwmars/README.md
@@ -0,0 +1,36 @@
+# Introduction
+
+Koha's Plugin System (available in Koha 3.12+) allows for you to add
+additional tools and reports to [Koha](http://koha-community.org) that
+are specific to your library. Plugins are installed by uploading KPZ (
+Koha Plugin Zip ) packages. A KPZ file is just a zip file containing
+the perl files, template files, and any other files necessary to make
+the plugin work. Learn more about the Koha Plugin System in the
+[Koha Wiki page about plugins](https://wiki.koha-community.org/wiki/Koha_plugins).
+
+# Installing
+
+The plugin system needs to be turned on by a system administrator.
+
+To set up the Koha plugin system you must first make some changes to your install.
+
+* Change `<enable_plugins>0<enable_plugins>` to `<enable_plugins>1</enable_plugins>` in your koha-conf.xml file
+* Confirm that the path to `<pluginsdir>` exists, is correct, and is writable by the web server
+* Restart your webserver
+
+Once set up is complete you will need to alter your UseKohaPlugins
+system preference.  On the Tools page you will see the Tools Plugins.
+From that page you can upload this plug (the .kpz file).  After that,
+the Format selector on the Stage MARC Records for Import page will
+give you the option "C/W MARS PlugIn".  This will allow you to import
+a simple text file, each line of which contains a link to a bibliographic
+record on the C/W MARS web site.  The plugin will read each such record
+from the web site, and convert it to a MARC record.
+
+You will also need the `cwmars.rb` script (in the top level of this
+git repository).  Copy this file to `/usr/local/bin` and make it
+executable.  You may need to edit the path in the first line to point
+to the actual location of the ruby executable in your system.
+Otherwise the plugin, which is running under Apache and which may not
+have the same PATH setting as your shell, may not be able to find
+ruby.
