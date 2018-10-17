2062dba06d460e2be17be00c85ef5bc97c63c3bf
marka@pobox.com <marka@pobox.com>
2018-10-17 15:00:44 +0000

    Add the URL suffix for a MARC view if the URL doesn't have it.
    FossilOrigin-Name: 3080958f12ca1d2e9473d5fe1fb808146fc4a6bf8cbd293a1ddf27be46890485

--- cwmars.rb
+++ cwmars.rb
@@ -2,7 +2,7 @@
 
 # This script reads a web page, either a local file or a URL, that should be
 # the page that is produced when you click on "Bibliographic Record" link on
-# an Evergreen (CWMARS) detail page for a book.  The resulting page should
+# an Evergreen (CWMARS, GAPINES, etc.) detail page for a book.  The resulting page should
 # have the book's MARC information, from which the script generates an
 # equivalent MARC record.  Here is an example of such a web page:
 # http://catalog.cwmars.org/eg/opac/record/982124?qtype=author;_adv=1;detail_record_view=0;fi%3Asearch_format=book;locg=1;query=Lessing Doris 1919 2013;expand=marchtml#marchtml
@@ -71,6 +71,9 @@ public
     @text = nil
 
     if filename =~ /^https?:/
+      if filename !~ /#marchtml$/
+	filename += ';expand=marchtml#marchtml'
+      end
       handle = IO.popen(['wget', '-nv', '-O', '-', filename])
     else
       handle = File.open(filename)
