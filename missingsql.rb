90d358fdc9f74bb8b9f60d855f82237607622158
marka@pobox.com <marka@pobox.com>
2020-03-26 17:52:17 +0000

    Add script to mark items in Koha as missing, given a CSV file produced by the Koha inventory tool.
    FossilOrigin-Name: b1ed87cced07d010039e06cdad4463c1c9d9330cb0883e111a30d111e94ca36a

---
+++ missingsql.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+# This script reads a CSV file containing the output of the Koha
+# inventory tool, and prints a list of SQL statements that can be used
+# to mark the items as missing. 
+
+require 'csv'
+require 'marc'
+
+if ARGV.length != 1
+   puts "usage: missingsql.rb inputfile.csv"
+   exit 1
+end
+input_file = ARGV[0]
+
+# Create a CSV file object, telling it the column separator character.
+f = File.open(input_file, "r")
+unless f
+  puts "Unable to open #{input_file}"
+  exit 1
+end
+
+csv = CSV.new(f, col_sep: ',')
+unless csv
+  puts "Unable to create a CSV object for #{input_file}"
+  exit 1
+end
+
+# Read the CSV file and output the SQL statements.
+first = true
+barcode_column = nil
+csv.each do |row|
+  # The first row contains column headers, from which we determine
+  # the index of the Barcode field.
+  if first
+    barcode_column = row.index('Barcode')
+    unless barcode_column
+      puts "Barcode not seen in first row"
+      exit 1
+    end
+    first = false
+  else
+    puts "update items set itemlost = 4 where barcode = '#{row[barcode_column]}';"
+  end
+end
